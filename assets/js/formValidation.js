ae8fc8218adc8761d8b4b8ac5ae811deada64b44
Gabriel <gabriel.gracilianomb@gmail.com>
2022-12-01 16:53:45 -0300

    Form is submitted through the fetch API
    - Removed Staticforms API to deal with the server side
    - Added FormSubmit API to submit the form
    - The page no longer reloads
    - Working for both pc and mobile

--- assets/js/formValidation.js
+++ assets/js/formValidation.js
@@ -94,10 +94,22 @@ const validateEmail = field => {
     return !validity;
 }
 
+const submitForm = async (name, email, subject, message) => {
+    fetch("https://formsubmit.co/ajax/0608572af44fdc8ec0fba1f58f12adfc", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({ name, email, subject, message })
+    });
+}
+
 export const customValidation = {
     errors,
     applyValidityClass,
     isEveryFieldValid,
     applyLabelOnTopEffect,
-    isEveryFieldEmpty
+    isEveryFieldEmpty,
+    submitForm
 }
--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,17 @@ const contactError = document.querySelector(".contact__error");
 const contactFields = document.querySelectorAll(".contact__field");
 const form = document.querySelector(".contact__form");
 
-window.onload = contactFields.forEach(field => field.value = "");
+const clearFields = () => contactFields.forEach(field => field.value = "");
+window.onload = clearFields;
+
+const resetFields = () => {
+    clearFields();
+    contactFields.forEach(field => {
+        field.focus();
+        field.blur();
+        field.classList.remove("contact__field--error");
+    })
+}
 
 contactFields.forEach(field => field.addEventListener("blur", event => {
   cv.applyLabelOnTopEffect(event);
@@ -15,10 +25,14 @@ contactFields.forEach(field => field.addEventListener("blur", event => {
 form.addEventListener("keyup", () => cv.isEveryFieldEmpty(contactFields, contactButton));
 
 contactButton.addEventListener("click", event => {
+    event.preventDefault();
     if(cv.isEveryFieldValid(contactFields)) {
+        const values = [];
+        contactFields.forEach(field => values.push(field.value));
+        cv.submitForm(...values);
+        resetFields();
         contactError.innerHTML = "";
-    } else {
-        event.preventDefault();
-        contactError.innerHTML = cv.errors.currentError;
+        return;
     }
+    contactError.innerHTML = cv.errors.currentError;
 });
