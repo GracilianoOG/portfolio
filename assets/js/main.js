9020f400035e27a9853f0ac822deb784370ab896
Gabriel <gabriel.gracilianomb@gmail.com>
2022-12-02 18:52:00 -0300

    Added modal feedback to the form
    - Success image added to be displayed
    - The whole form will be covered by a modal thanking the user
    - The user can close the modal to send more messages

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,11 +3,20 @@ import { formHandling as fh } from "./formHandling.js";
 
 const contactButton = document.querySelector(".contact__button");
 const contactError = document.querySelector(".contact__error");
+const contactFeedback = document.querySelector(".contact__feedback");
 const contactFields = document.querySelectorAll(".contact__field");
 const form = document.querySelector(".contact__form");
 
 const clearFields = () => contactFields.forEach(field => field.value = "");
 window.onload = clearFields;
+
+const showFeedback = () => {
+    contactFeedback.classList.add("contact__feedback--show");
+}
+
+const closeFeedback = () => {
+    contactFeedback.classList.remove("contact__feedback--show");
+}
 
 const resetFields = () => {
     clearFields();
@@ -18,19 +27,22 @@ const resetFields = () => {
     })
 }
 
+contactFeedback.addEventListener("click", closeFeedback);
+
 contactFields.forEach(field => field.addEventListener("blur", event => {
     fv.applyLabelOnTopEffect(event);
     fv.applyValidityClass(event);
 }));
 
 form.addEventListener("keyup", () => fv.isEveryFieldEmpty(contactFields, contactButton));
-
+-
 contactButton.addEventListener("click", event => {
     event.preventDefault();
     if(fv.isEveryFieldValid(contactFields)) {
         const values = [];
         contactFields.forEach(field => values.push(field.value));
         fh.submitForm(...values);
+        showFeedback();
         resetFields();
         contactError.innerHTML = "";
         return;
