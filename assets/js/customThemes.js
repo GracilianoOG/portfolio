257e46367bd3b43dad62d00f604efcd7d0f740c3
Gabriel <gabriel.gracilianomb@gmail.com>
2024-11-15 18:28:12 -0300

    Improve theme script

--- assets/js/customThemes.js
+++ assets/js/customThemes.js
@@ -2,20 +2,19 @@ const classList = document.documentElement.classList;
 const themes = { key: "theme", light: "light", dark: "dark" };
 
 const toggleTheme = () => {
-  if(classList.length > 0) {
-    classList.toggle(themes.dark);
-    classList.toggle(themes.light);
-  } else {
-    const isDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
-    classList.add(isDark ? themes.light : themes.dark);
-  }
-
+  classList.toggle(themes.dark);
+  classList.toggle(themes.light);
   sessionStorage.setItem(themes.key, classList.contains(themes.light) ? themes.light : themes.dark);
 }
 
 export const loadCachedTheme = () => {
   const cachedTheme = sessionStorage.getItem(themes.key);
-  if(cachedTheme) classList.add(cachedTheme);
+  if(cachedTheme) {
+    classList.add(cachedTheme);
+  } else {
+    const isDarkPreferred = window.matchMedia("(prefers-color-scheme: dark)").matches;
+    classList.add(isDarkPreferred ? themes.dark : themes.light);
+  }
 }
 
 export default toggleTheme;
