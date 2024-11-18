e0767da41fa57a50e28fd0e6fbfe90d2234a16cd
Gabriel <gabriel.gracilianomb@gmail.com>
2024-11-18 14:22:56 -0300

    Change toggler pressed state when interacted with

--- assets/js/customThemes.js
+++ assets/js/customThemes.js
@@ -8,6 +8,12 @@ export const setupThemeToggler = () => {
     classList.toggle(themes.dark);
     classList.toggle(themes.light);
     sessionStorage.setItem(themes.key, classList.contains(themes.light) ? themes.light : themes.dark);
+
+    if(classList.contains(themes.light)) {
+      themeToggler.setAttribute("aria-pressed", "false");
+    } else {
+      themeToggler.setAttribute("aria-pressed", "true");
+    }
   });
 }
 
@@ -15,4 +21,12 @@ export const loadStoredTheme = () => {
   const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
   const cachedTheme = sessionStorage.getItem(themes.key) || (prefersDark ? themes.dark : themes.light);
   classList.add(cachedTheme);
+  console.log(prefersDark)
+
+  const themeToggler = document.querySelector(".theme-toggler");
+  if(cachedTheme === themes.light) {
+    themeToggler.setAttribute("aria-pressed", "false");
+  } else {
+    themeToggler.setAttribute("aria-pressed", "true");
+  }
 }
