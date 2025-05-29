14466f871c825fccb00463d68054e8a1ac1d40b4
Gabriel <gabriel.gracilianomb@gmail.com>
2025-05-29 20:39:24 -0300

    Format main

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,53 +12,57 @@ const contactFields = document.querySelectorAll(".contact__field");
 const form = document.querySelector(".contact__form");
 
 // Clear all input fields
-const clearFields = () => contactFields.forEach(field => field.value = "");
+const clearFields = () => contactFields.forEach(field => (field.value = ""));
 window.onload = clearFields;
 
 // Manipulate the feedback modal
 const showFeedback = () => {
-    contactFeedback.classList.add("contact__feedback--show");
-}
+  contactFeedback.classList.add("contact__feedback--show");
+};
 
 const closeFeedback = () => {
-    contactFeedback.classList.remove("contact__feedback--show");
-}
+  contactFeedback.classList.remove("contact__feedback--show");
+};
 
 // Reset all the styles of the fields
 const resetFields = () => {
-    clearFields();
-    contactFields.forEach(field => {
-        field.focus();
-        field.blur();
-        field.classList.remove("contact__field--error");
-    })
-}
+  clearFields();
+  contactFields.forEach(field => {
+    field.focus();
+    field.blur();
+    field.classList.remove("contact__field--error");
+  });
+};
 
 // Closable contact modal
 contactFeedback.addEventListener("click", closeFeedback);
 
 // Add classes on the fields
-contactFields.forEach(field => field.addEventListener("blur", event => {
+contactFields.forEach(field =>
+  field.addEventListener("blur", event => {
     fv.applyLabelOnTopEffect(event);
     fv.applyValidityClass(event);
-}));
+  })
+);
 
 // Disable submit button if it's empty
-form.addEventListener("keyup", () => fv.isEveryFieldEmpty(contactFields, contactButton));
+form.addEventListener("keyup", () =>
+  fv.isEveryFieldEmpty(contactFields, contactButton)
+);
 
 // Show a message after submiting the form
 contactButton.addEventListener("click", event => {
-    event.preventDefault();
-    if(fv.isEveryFieldValid(contactFields)) {
-        const values = [];
-        contactFields.forEach(field => values.push(field.value));
-        fh.submitForm(...values);
-        showFeedback();
-        resetFields();
-        contactError.innerHTML = "";
-        return;
-    }
-    contactError.innerHTML = fv.errors.currentError;
+  event.preventDefault();
+  if (fv.isEveryFieldValid(contactFields)) {
+    const values = [];
+    contactFields.forEach(field => values.push(field.value));
+    fh.submitForm(...values);
+    showFeedback();
+    resetFields();
+    contactError.innerHTML = "";
+    return;
+  }
+  contactError.innerHTML = fv.errors.currentError;
 });
 
 loadStoredTheme();
