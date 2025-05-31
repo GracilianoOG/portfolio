298baf64e2b8ab8eb7d2e942153dc731649039c0
Gabriel <gabriel.gracilianomb@gmail.com>
2025-05-31 13:02:52 -0300

    Re-write formValidation
    - File named changed to plural form.
    - Reduced from 11 to 5 functions so far.
    - Reduced exported functions.
    - Cleaner structure.

---
+++ src/js/formValidations.js
@@ -0,0 +1,90 @@
+const fields = {
+  name: document.querySelector("#name"),
+  email: document.querySelector("#email"),
+  subject: document.querySelector("#subject"),
+  message: document.querySelector("#message"),
+};
+
+const validateFields = () => {
+  const validations = {
+    name: [
+      {
+        valid: !fields.name.validity.valueMissing,
+        message: "Name input is empty",
+      },
+      {
+        valid: !fields.name.validity.tooLong,
+        message: "Name input is too long",
+      },
+    ],
+    email: [
+      {
+        valid: !fields.email.validity.valueMissing,
+        message: "Email input is empty",
+      },
+      {
+        valid: !fields.email.validity.typeMismatch,
+        message: "Provide a valid email format",
+      },
+    ],
+    subject: [
+      {
+        valid: !fields.subject.validity.valueMissing,
+        message: "Subject input is empty",
+      },
+      {
+        valid: !fields.subject.validity.tooLong,
+        message: "Subject input is too long",
+      },
+    ],
+    message: [
+      {
+        valid: !fields.message.validity.valueMissing,
+        message: "Message input is empty",
+      },
+      {
+        valid: !fields.message.validity.tooLong,
+        message: "Message input is too long",
+      },
+    ],
+  };
+
+  return validations;
+};
+
+const isFieldValid = name => {
+  const validations = validateFields();
+  return validations[name].every(({ valid }) => valid);
+};
+
+export const applyValidityStyle = field => {
+  const isValid = isFieldValid(field.name);
+  const ERROR_CLASS = "contact__field--error";
+  const VALID_CLASS = "contact__field--ok";
+  field.classList.add(isValid ? VALID_CLASS : ERROR_CLASS);
+  field.classList.remove(isValid ? ERROR_CLASS : VALID_CLASS);
+};
+
+export const keepLabelOnTop = field => {
+  const HAS_CONTENT_CLASS = "has-content";
+  const length = field.value.length;
+  length > 0
+    ? field.classList.add(HAS_CONTENT_CLASS)
+    : field.classList.remove(HAS_CONTENT_CLASS);
+};
+
+export const validateForm = () => {
+  const validations = validateFields();
+  const validation = [true, ""];
+
+  Object.values(validations).forEach(field => {
+    field.forEach(({ valid, message }) => {
+      if (!valid) {
+        validation[0] = false;
+        validation[1] += `<p>${message}!</p>`;
+      }
+    });
+  });
+
+  return validation;
+};
