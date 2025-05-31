ebd0090e2eac340f913a418e78a69a62e5f386fd
Gabriel <gabriel.gracilianomb@gmail.com>
2025-05-31 13:20:26 -0300

    Export submitForm directly

--- src/js/formHandling.js
+++ src/js/formHandling.js
@@ -1,14 +1,10 @@
-const submitForm = async (name, email, subject, message) => {
+export const submitForm = async (name, email, subject, message) => {
   fetch("https://formsubmit.co/ajax/0608572af44fdc8ec0fba1f58f12adfc", {
-      method: "POST",
-      headers: {
-          "Content-Type": "application/json",
-          "Accept": "application/json"
-      },
-      body: JSON.stringify({ name, email, subject, message })
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({ name, email, subject, message }),
   });
-}
-
-export const handling = {
-  submitForm
-}
+};
