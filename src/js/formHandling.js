42e096f7f088c40ae1fe2dfbef080d74c2d17e3b
Gabriel <gabriel.gracilianomb@gmail.com>
2025-05-31 17:57:19 -0300

    Handle possible errors after submission

--- src/js/formHandling.js
+++ src/js/formHandling.js
@@ -1,10 +1,20 @@
 export const submitForm = async (name, email, subject, message) => {
-  fetch("https://formsubmit.co/ajax/0608572af44fdc8ec0fba1f58f12adfc", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json",
-    },
-    body: JSON.stringify({ name, email, subject, message }),
-  });
+  try {
+    const url = "https://formsubmit.co/ajax/0608572af44fdc8ec0fba1f58f12adfc";
+    const resp = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({ name, email, subject, message }),
+    });
+
+    if (!resp.ok) {
+      throw new Error(`HTTP error! status: ${resp.status}`);
+    }
+  } catch (err) {
+    console.info("An error has happened during the form submission.");
+    console.error(err.message);
+  }
 };
