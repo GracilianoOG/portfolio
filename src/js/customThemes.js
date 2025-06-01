e2256be4d491cf842c08e84d3ce0381b517764c1
Gabriel <gabriel.gracilianomb@gmail.com>
2025-06-01 14:32:15 -0300

    Refactor customThemes

--- src/js/customThemes.js
+++ src/js/customThemes.js
@@ -1,22 +1,25 @@
-export const setupThemeToggler = () => {
-  const Themes = Object.freeze({ LIGHT: "light", DARK: "dark" });
-  const themeToggler = document.querySelector(".theme-toggler");
-  const classList = document.documentElement.classList;
-  const lamp = document.querySelector("#lampAudio");
+const Themes = Object.freeze({ LIGHT: "light", DARK: "dark" });
+const themeToggler = document.querySelector(".theme-toggler");
+const classList = document.documentElement.classList;
+const lamp = document.querySelector("#lampAudio");
 
-  themeToggler.addEventListener("click", () => {
-    classList.toggle(Themes.DARK);
-    classList.toggle(Themes.LIGHT);
-    const isLightTheme = classList.contains(Themes.LIGHT);
-    localStorage.setItem("theme", isLightTheme ? Themes.LIGHT : Themes.DARK);
-    themeToggler.setAttribute("aria-pressed", isLightTheme ? "false" : "true");
-    lamp.pause();
-    lamp.currentTime = 0;
-    lamp.play();
-  });
-
-  themeToggler.setAttribute(
-    "aria-pressed",
-    classList.contains(Themes.LIGHT) ? "false" : "true"
-  );
+const handleSound = () => {
+  lamp.pause();
+  lamp.currentTime = 0;
+  lamp.play();
 };
+
+const changeTheme = () => {
+  classList.toggle(Themes.DARK);
+  classList.toggle(Themes.LIGHT);
+  const isLightTheme = classList.contains(Themes.LIGHT);
+  localStorage.setItem("theme", isLightTheme ? Themes.LIGHT : Themes.DARK);
+  themeToggler.setAttribute("aria-pressed", isLightTheme ? "false" : "true");
+  handleSound();
+};
+
+themeToggler.addEventListener("click", changeTheme);
+themeToggler.setAttribute(
+  "aria-pressed",
+  classList.contains(Themes.LIGHT) ? "false" : "true"
+);
