3752743e3ba51d2cf8690b563b1704493456ce16
Gabriel <gabriel.gracilianomb@gmail.com>
2025-06-07 19:53:29 -0300

    Load env variables

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
 import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 
-export default defineConfig({
-  plugins: [
-    ViteImageOptimizer({
-      png: { quality: 80 },
-      jpeg: { quality: 80 },
-    }),
-  ],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    plugins: [
+      ViteImageOptimizer({
+        png: { quality: 80 },
+        jpeg: { quality: 80 },
+      }),
+    ],
+    define: {
+      "process.env.FORMSUBMIT_KEY": JSON.stringify(env.FORMSUBMIT_KEY),
+    },
+  };
 });
