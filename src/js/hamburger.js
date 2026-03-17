67815c0e96c7a0f36bb03b2c4d895c368bb5f3f2
Gabriel <gabriel.gracilianomb@gmail.com>
2026-03-17 15:42:42 -0300

    Remove lines that change hamburger label

--- src/js/hamburger.js
+++ src/js/hamburger.js
@@ -5,11 +5,9 @@ hamburger.addEventListener("click", () => {
   headerMenu.classList.toggle("header__menu--off");
   hamburger.classList.toggle("header__hamburguer--close");
 
-  if(headerMenu.classList.contains("header__menu--off")) {
+  if (headerMenu.classList.contains("header__menu--off")) {
     hamburger.setAttribute("aria-expanded", "false");
-    hamburger.setAttribute("aria-label", "Open navigation menu");
   } else {
     hamburger.setAttribute("aria-expanded", "true");
-    hamburger.setAttribute("aria-label", "Close navigation menu");
   }
 });
