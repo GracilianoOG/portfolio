807a564e2a81909a0f4b2b26a15a1983b9e106d3
Gabriel <gabriel.gracilianomb@gmail.com>
2026-03-17 15:50:18 -0300

    Wrap menu logic in a function

--- src/js/hamburger.js
+++ src/js/hamburger.js
@@ -1,9 +1,11 @@
 const hamburger = document.querySelector(".header__hamburguer");
 const headerMenu = document.querySelector(".header__menu");
 
-hamburger.addEventListener("click", () => {
+const toggleHamburgerMenu = () => {
   headerMenu.classList.toggle("header__menu--off");
   hamburger.classList.toggle("header__hamburguer--close");
   const isMenuOpen = hamburger.classList.contains("header__hamburguer--close");
   hamburger.setAttribute("aria-expanded", isMenuOpen);
-});
+};
+
+hamburger.addEventListener("click", toggleHamburgerMenu);
