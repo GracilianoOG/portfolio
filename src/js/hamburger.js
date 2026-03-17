f1dd258d6f3eb7a7b018456d33ee22526dc6dd79
Gabriel <gabriel.gracilianomb@gmail.com>
2026-03-17 15:47:52 -0300

    Reduce lines to change hamburger state

--- src/js/hamburger.js
+++ src/js/hamburger.js
@@ -4,10 +4,6 @@ const headerMenu = document.querySelector(".header__menu");
 hamburger.addEventListener("click", () => {
   headerMenu.classList.toggle("header__menu--off");
   hamburger.classList.toggle("header__hamburguer--close");
-
-  if (headerMenu.classList.contains("header__menu--off")) {
-    hamburger.setAttribute("aria-expanded", "false");
-  } else {
-    hamburger.setAttribute("aria-expanded", "true");
-  }
+  const isMenuOpen = hamburger.classList.contains("header__hamburguer--close");
+  hamburger.setAttribute("aria-expanded", isMenuOpen);
 });
