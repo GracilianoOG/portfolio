d536ec204a2641c4bf67719e48b7c7d401c9c6ee
Gabriel <gabriel.gracilianomb@gmail.com>
2026-03-17 16:08:23 -0300

    Fix name of hamburger class to get the button

--- src/js/hamburger.js
+++ src/js/hamburger.js
@@ -1,10 +1,10 @@
-const hamburger = document.querySelector(".header__hamburguer");
+const hamburger = document.querySelector(".hamburger");
 const headerMenu = document.querySelector(".header__menu");
 
 const toggleHamburgerMenu = () => {
   headerMenu.classList.toggle("header__menu--off");
-  hamburger.classList.toggle("header__hamburguer--close");
-  const isMenuOpen = hamburger.classList.contains("header__hamburguer--close");
+  hamburger.classList.toggle("hamburger--close");
+  const isMenuOpen = hamburger.classList.contains("hamburger--close");
   hamburger.setAttribute("aria-expanded", isMenuOpen);
 };
 
