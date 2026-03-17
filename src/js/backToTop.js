4a0244e7cee668fc7c7dbceddc0f4557c3e5ae3e
Gabriel <gabriel.gracilianomb@gmail.com>
2026-03-17 15:53:26 -0300

    Extract onScroll logic to a function

--- src/js/backToTop.js
+++ src/js/backToTop.js
@@ -1,10 +1,15 @@
 const btn = document.querySelector(".to-top");
 const amount = 150;
 
-window.onscroll = () => {
-  if(document.body.scrollTop >= amount || document.documentElement.scrollTop >= amount) {
+const changeVisibilityOnScroll = () => {
+  if (
+    document.body.scrollTop >= amount ||
+    document.documentElement.scrollTop >= amount
+  ) {
     btn.classList.remove("to-top--hidden");
   } else {
     btn.classList.add("to-top--hidden");
   }
-}
+};
+
+window.onscroll = changeVisibilityOnScroll;
