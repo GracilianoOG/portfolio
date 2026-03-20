dab5dbb1e42a91ffae757589958df023df1a64ee
Gabriel <gabriel.gracilianomb@gmail.com>
2026-03-20 12:52:51 -0300

    Move cards to a new js file

--- src/js/projectCards.js
+++ src/js/projectCards.js
@@ -1,44 +1,6 @@
-import img1 from "../assets/img/projects/project-1.png";
-import img2 from "../assets/img/projects/project-2.png";
-import img3 from "../assets/img/projects/project-3.png";
+import { cards } from "./cards";
 
 const projectsEl = document.querySelector("#projects");
-
-const cards = [
-  {
-    title: "Product Card",
-    description:
-      "This is a responsive Product Card made with HTML and plain CSS. I had a lot of fun and learnt a lot while developing it.",
-    tags: ["html", "css"],
-    links: {
-      source: "https://github.com/GracilianoOG/fementor-product-card",
-      preview: "https://gracilianoog.github.io/fementor-product-card/",
-    },
-    imgURL: img1,
-  },
-  {
-    title: "QR Code",
-    description:
-      "This is a simple QR Code Preview made with HTML and CSS. It is simple, but I really enjoyed making it.",
-    tags: ["html", "css"],
-    links: {
-      source: "https://github.com/GracilianoOG/fementor-qrcode",
-      preview: "https://gracilianoog.github.io/fementor-qrcode/",
-    },
-    imgURL: img2,
-  },
-  {
-    title: "Text Encoder",
-    description:
-      "I made it during the Oracle Next Education course through the Alura platform. I learnt so much coding it! It was made with HTML, CSS and Javascript.",
-    tags: ["html", "css", "javascript"],
-    links: {
-      source: "https://github.com/GracilianoOG/descodificador-de-texto",
-      preview: "https://gracilianoog.github.io/descodificador-de-texto/",
-    },
-    imgURL: img3,
-  },
-];
 
 projectsEl.innerHTML += cards
   .map((card, index) => {
