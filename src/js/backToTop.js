d855f54db917839015cc60347e3f592ab075bc4a
Gabriel <gabriel.gracilianomb@gmail.com>
2026-05-10 22:28:40 -0300

    Move amount to function's parameters

--- src/js/backToTop.js
+++ src/js/backToTop.js
@@ -1,11 +1,10 @@
 const btn = document.querySelector(".to-top");
 
-const changeVisibilityOnScroll = () => {
-  const amount = 150;
+const changeVisibilityOnScroll = (amount = 150) => {
   const isNotStuck =
     document.body.scrollTop >= amount ||
     document.documentElement.scrollTop >= amount;
   btn.classList.toggle("to-top--hidden", !isNotStuck);
 };
 
-window.onscroll = changeVisibilityOnScroll;
+window.onscroll = () => changeVisibilityOnScroll();
