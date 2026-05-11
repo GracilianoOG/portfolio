c00eae0a30e83c30a89851f3e6ed506ce1c534fc
Gabriel <gabriel.gracilianomb@gmail.com>
2026-05-10 22:36:04 -0300

    Turn isMenuOpen into a function
    Improved readability and logic separation.

--- src/js/hamburger.js
+++ src/js/hamburger.js
@@ -1,11 +1,12 @@
 const hamburger = document.querySelector(".hamburger");
 const headerMenu = document.querySelector(".header__menu");
 
+const isMenuOpen = () => hamburger.classList.contains("hamburger--close");
+
 const toggleHamburgerMenu = () => {
   headerMenu.classList.toggle("header__menu--off");
   hamburger.classList.toggle("hamburger--close");
-  const isMenuOpen = hamburger.classList.contains("hamburger--close");
-  hamburger.setAttribute("aria-expanded", isMenuOpen);
+  hamburger.setAttribute("aria-expanded", isMenuOpen());
 };
 
 hamburger.addEventListener("click", toggleHamburgerMenu);
