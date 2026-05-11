4cba7397ab72dcfecb61bbb9c0b95a3fa34d3f00
Gabriel <gabriel.gracilianomb@gmail.com>
2026-05-11 18:43:29 -0300

    Rename function to handleScroll

--- src/js/backToTop.js
+++ src/js/backToTop.js
@@ -1,8 +1,8 @@
 const btn = document.querySelector(".to-top");
 
-const changeVisibilityOnScroll = (element, amount = 150) => {
+const handleScroll = (element, amount = 150) => {
   const isNotStuck = window.scrollY >= amount;
   element.classList.toggle("to-top--hidden", !isNotStuck);
 };
 
-window.onscroll = () => changeVisibilityOnScroll(btn);
+window.onscroll = () => handleScroll(btn);
