6744611b7eb3c2a9ecbf9e98c25283306c8ce693
Gabriel <gabriel.gracilianomb@gmail.com>
2026-05-11 23:10:13 -0300

    Convert returned value of isMenuOpen to string

--- src/js/hamburger.ts
+++ src/js/hamburger.ts
@@ -6,7 +6,7 @@ const isMenuOpen = () => hamburger.classList.contains("hamburger--close");
 const toggleHamburgerMenu = () => {
   headerMenu.classList.toggle("header__menu--off");
   hamburger.classList.toggle("hamburger--close");
-  hamburger.setAttribute("aria-expanded", isMenuOpen());
+  hamburger.setAttribute("aria-expanded", String(isMenuOpen()));
 };
 
 hamburger.addEventListener("click", toggleHamburgerMenu);
